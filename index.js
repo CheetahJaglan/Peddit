fad12b429cb640766340c0b0a936380707a364e7
CheetahJaglan <cheetahjaglan@gmail.com>
2025-03-08 19:30:03 +0530

    Make the homepage

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const port = 3000;
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-
+const posts = JSON.parse(fs.readFileSync(path.join(__dirname, 'data.json'), 'utf8'));
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine','ejs');
-
+app.get('/posts', (req, res) => {
+    res.render('posts', { posts: posts.posts }); // Send only the array
+});
 app.get('/',(req,res) => {
     res.send("code working perfectly");
 });
