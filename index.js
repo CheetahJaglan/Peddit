dc7fcd80bdf27b2d5e24cc9118e850fa8a3f3338
CheetahJaglan <cheetahjaglan@gmail.com>
2025-03-08 21:15:36 +0530

    Added the ability for the user to post a post

--- index.js
+++ index.js
@@ -9,13 +9,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine','ejs');
+
 app.get('/posts', (req, res) => {
     res.render('posts', { posts: posts.posts }); // Send only the array
 });
+app.post('/posts', (req, res) => {
+    console.log(req.body);
+    let { username, title, content } = req.body;
+    let id = uuidv4();
+    
+    posts.posts.push({ id, username, title, content });
+    
+    fs.writeFileSync(
+        path.join(__dirname, 'data.json'),
+        JSON.stringify(posts, null, 2)
+    );
+    
+    res.redirect('/posts');
+});
+
 app.get('/',(req,res) => {
-    res.send("code working perfectly");
+    res.redirect("./posts")
+});
+app.get('/make_a_post',(req,res) => {
+    res.render('send_form')
 });
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-    console.log(posts)
 });
