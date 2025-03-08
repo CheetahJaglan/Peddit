7f29bbad09c540b5edde1b5784fed3c132c9068e
CheetahJaglan <cheetahjaglan@gmail.com>
2025-03-08 19:31:31 +0530

    Added the styling

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { v4: uuidv4 } = require('uuid');
 const posts = JSON.parse(fs.readFileSync(path.join(__dirname, 'data.json'), 'utf8'));
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine','ejs');
 app.get('/posts', (req, res) => {
     res.render('posts', { posts: posts.posts }); // Send only the array
