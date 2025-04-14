5807cc90f18293b092e1578321b48df25040ad0c
CheetahJaglan <cheetahjaglan@gmail.com>
2025-04-14 16:11:27 +0530

    basic cloud connection

--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,13 @@ const express = require('express');
 const User = require('../models/user');
 const passport = require('passport');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const {storage} = require('../cloudConfig.js');
+const upload = multer({storage});
 const router = express.Router();
 
 
 // router
-//     .route('/')
-//     .post(upload.single('ProfilePic'), (req, res) => {
-//         res.send(req.file);
-//     })
+//   
 
 // Example route: GET /signup
 router.get('/signup', (req, res) => {
@@ -22,8 +20,6 @@ router.post('/signup', upload.single('ProfilePic') , async (req, res) => {
     try{
         let {username, password, bio} = req.body;
         const profilePicPath = req.file ? `/uploads/${req.file.filename}` : null;
-        console.log('Profile Pic Path:', req.file);
-        console.log('Body:', req.body);
         const newUser = new User({username, password, bio, profilePic: profilePicPath});
         await User.register(newUser, password);
         req.flash("success", "User Registered");
